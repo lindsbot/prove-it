044a82fb37e1152709422ea15678f3972be143e3
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-10-27 17:23:05 -0700

    [wip] separating multiple choice from fill in questions on control scope

--- public/js/controllers/quiz.js
+++ public/js/controllers/quiz.js
@@ -3,8 +3,17 @@ angular.module('mean.system').controller('QuizController', ['$scope', '$http', '
 
   $http.get('http://localhost:3000/quizData?callback=JSON_CALLBACK')
     .success(function(data, status, headers, config){
-      $scope.quiz = data.questions;
-      console.log("hooray!");
+      $scope.quiz = {};
+      $scope.quiz.multipleChoice = [];
+      $scope.quiz.fillIn = [];
+      for (var i = 0; i < data.questions.length; i++) {
+        if (data.questions[i].type === "multiple_choice") {
+          $scope.quiz.multipleChoice.push(data.questions[i]);
+        }
+        else if (data.questions[i].type === "fill_in") {
+          $scope.quiz.fillIn.push(data.questions[i]);
+        }
+      }
     })
     .error(function(data, status, headers, config){
       console.log("error </3 ");
