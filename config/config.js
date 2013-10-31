fa7951a44c521423b4f34e512d540b1c15009cb6
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-10-31 08:20:40 -0700

    deploying

--- config/config.js
+++ config/config.js
@@ -6,3 +6,4 @@ module.exports = _.extend(
     require(__dirname + '/../config/env/all.js'),
     require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {},
     db: process.env.MONGOLAB_URI);
+
