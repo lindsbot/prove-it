64f816e339898625a22f5cb81516a2999b034495
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-10-31 08:17:24 -0700

    deploying, added .env to gitignore

--- config/config.js
+++ config/config.js
@@ -4,4 +4,5 @@ var _ = require('underscore');
 
 module.exports = _.extend(
     require(__dirname + '/../config/env/all.js'),
-    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {});
+    require(__dirname + '/../config/env/' + process.env.NODE_ENV + '.json') || {},
+    db: process.env.MONGOLAB_URI);
