e0b4166138d869ed3fd589dd57d2297638f32b00
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-11-02 15:48:57 -0700

    adding tests

--- test/e2e/scenario.js
+++ test/e2e/scenario.js
@@ -1,3 +1,18 @@
 describe('quiz app', function(){
+
+  it('should load the index', function(){
+    browser().navigateTo('../../views/index.html');
+    expect(browser().location().url()).toBe('/');
+  });
+
+  it('should load header', function(){
+    browser().navigateTo('/quiz');
+    expect(element('h2').text().toMatch('Take this AngularJS quiz to prove it'));
+  });
+
+  it('should load first question', function(){
+    browser().navigateTo('/quiz');
+    expect(element('.question').text().toMatch('Which is not an advantage of using a closure?'));
+  });
   
 });
