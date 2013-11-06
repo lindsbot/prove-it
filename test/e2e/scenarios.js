e2d306b1d5b1bf9754f4e7f9203bcdf91001853e
Lindsay Hertz <lindsay.hertz@gmail.com>
2013-11-06 12:14:39 -0800

    added tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -1,12 +1,12 @@
 describe('quiz app', function(){
 
-  it('should load the index', function(){
+  beforeEach(function(){
     browser().navigateTo('../../views/index.html');
-    expect(browser().location().url()).toBe('/');
   });
 
   it('should load header', function(){
     browser().navigateTo('/quiz');
+    expect(browser().location().url()).toBe('/quiz');
     expect(element('h2').text().toMatch('Take this AngularJS quiz to prove it'));
   });
 
@@ -14,5 +14,13 @@ describe('quiz app', function(){
     browser().navigateTo('/quiz');
     expect(element('.question').text().toMatch('Which is not an advantage of using a closure?'));
   });
+
+  it('should select an answer option', function(){
+    browser().navigateTo('/quiz');
+    input('$parent.question.selected').select('Prevent pollution of global scope');
+    expect(binding('$parent.question.selected')).toEqual('Prevent pollution of global scope');
+  });
+
+
   
 });
